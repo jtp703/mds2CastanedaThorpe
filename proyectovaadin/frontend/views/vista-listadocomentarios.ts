c2094779f4d5bd7a30b7bff68d0cd79172da5f79
DIEGO CASTAÑEDA <dcc023@inlumine.ual.es>
2025-03-30 13:54:57 +0200

    Listadocomentarios terminado

--- proyectovaadin/frontend/views/vista-listadocomentarios.ts
+++ proyectovaadin/frontend/views/vista-listadocomentarios.ts
@@ -1,7 +1,7 @@
 import { LitElement, html, css, customElement } from 'lit-element';
 import './vista-listadotweets_item';
-import './vista-listadocomentarios_item';
 import '@vaadin/vertical-layout/src/vaadin-vertical-layout.js';
+import './vista-listadocomentarios_item';
 import '@vaadin/horizontal-layout/src/vaadin-horizontal-layout.js';
 
 @customElement('vista-listadocomentarios')
@@ -18,19 +18,15 @@ export class VistaListadocomentarios extends LitElement {
   render() {
     return html`
 <vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%; position:absolute;" id="vaadinVerticalLayout">
- <vaadin-vertical-layout theme="spacing" style="width: 100%; flex-shrink: 0; flex-grow: 1; flex-direction: column;" id="vaadinVerticalLayout1">
-  <vaadin-horizontal-layout theme="spacing" style="width: 100%; flex-shrink: 0; flex-grow: 1; flex-direction: column;" id="vaadinHorizontalLayout">
-   <vista-listadotweets_item style="flex-shrink: 0;" id="vistaListadotweets_item"></vista-listadotweets_item>
+ <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; flex-grow: 1;" id="vaadinHorizontalLayout">
+  <vista-listadotweets_item style="flex-shrink: 0;" id="vistaListadotweets_item"></vista-listadotweets_item>
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; width: 100%;">
+  <vaadin-horizontal-layout theme="spacing" style="flex-grow: 0; flex-shrink: 0; width: 100%;" id="vaadinHorizontalLayout3">
+   <vista-listadocomentarios_item style="flex-shrink: 1;" id="vistaListadocomentarios_item"></vista-listadocomentarios_item>
   </vaadin-horizontal-layout>
-  <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; flex-grow: 0; width: 100%;" id="vaadinHorizontalLayout1">
-   <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; flex-grow: 1;" id="vaadinVerticalLayout2">
-    <vaadin-horizontal-layout theme="spacing" style="width: 100%; flex-grow: 0; flex-shrink: 0;" id="vaadinHorizontalLayout3">
-     <vista-listadocomentarios_item style="width: 100%; flex-shrink: 0;" id="vistaListadocomentarios_item"></vista-listadocomentarios_item>
-    </vaadin-horizontal-layout>
-    <vaadin-horizontal-layout theme="spacing" style="width: 100%; flex-grow: 0; flex-shrink: 0;" id="vaadinHorizontalLayout4">
-     <vista-listadocomentarios_item style="width: 100%; flex-shrink: 0;" id="vistaListadocomentarios_item1"></vista-listadocomentarios_item>
-    </vaadin-horizontal-layout>
-   </vaadin-vertical-layout>
+  <vaadin-horizontal-layout theme="spacing" style="flex-grow: 0; flex-shrink: 0; width: 100%;" id="vaadinHorizontalLayout4">
+   <vista-listadocomentarios_item style="flex-shrink: 1;" id="vistaListadocomentarios_item1"></vista-listadocomentarios_item>
   </vaadin-horizontal-layout>
  </vaadin-vertical-layout>
 </vaadin-vertical-layout>
